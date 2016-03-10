0e7c340d83d2452979c455e8a8cc8da4b2d31824
giadarol <giovanni.iadarola@cern.ch>
2016-03-10 23:16:24 +0100

    Started ring with clouds
    But slice info not included in communication helper...
    It is the next step.

--- test_ring_with_buffers/001_ring_with_clouds.py
+++ test_ring_with_buffers/001_ring_with_clouds.py
@@ -4,16 +4,18 @@ sys.path.append(BIN)
 BIN=os.path.expanduser('../')
 sys.path.append(BIN)
 
+sys.path.append(BIN)
+
 import numpy as np
-from scipy.constants import c
+import time
+from scipy.constants import c,e
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 
 import communication_helpers as ch
 
-
 macroparticlenumber_track = 50000
-N_turns = 128
+N_turns = 2
 
 epsn_x  = 2.5e-6
 epsn_y  = 3.5e-6
@@ -21,8 +23,30 @@ sigma_z = 0.05
 
 intensity = 1e11
 
-n_slices = 100
+n_slices = 150
 z_cut = 2.5e-9*c
+
+
+# config e-cloud
+chamb_type = 'polyg'
+x_aper = 2.300000e-02
+y_aper = 1.800000e-02
+filename_chm = '../pyecloud_config/LHC_chm_ver.mat'
+B_multip_per_eV = [1.190000e-12]
+B_multip_per_eV = np.array(B_multip_per_eV)
+
+fraction_device = 0.65
+intensity = 1.150000e+11
+epsn_x = 2.5e-6
+epsn_y = 2.5e-6
+init_unif_edens_flag = 1
+init_unif_edens = 9.000000e+11
+N_MP_ele_init = 100000
+N_mp_max = N_MP_ele_init*4.
+Dh_sc = .2e-3
+nel_mp_ref_0 = init_unif_edens*4*x_aper*y_aper/N_MP_ele_init
+
+n_segments = 4
 
 
 # get info on the grid
@@ -39,9 +63,10 @@ buf_float = np.array(N_buffer_float_size*[0.])
 N_buffer_int_size = 10
 buf_int = np.array(N_buffer_int_size*[0])
 
-		
+print 'I am %d of %d'%(myid, N_nodes)	
+	
 from LHC import LHC
-machine = LHC(machine_configuration='Injection', n_segments=43, D_x=0., 
+machine = LHC(machine_configuration='Injection', n_segments=n_segments, 
 				RF_at='end_of_transverse')
 
 # We suppose that all the object that cannot be slice parallelized are at the end of the ring
@@ -57,9 +82,9 @@ if I_am_a_worker:
 	mypart = machine.one_turn_map[N_elements_per_worker*myid:N_elements_per_worker*(myid+1)]
 	print 'I am id=%d and my part is %d long'%(myid, len(mypart))
 else: 	
-	part_for_master = machine.one_turn_map[N_elements_per_worker*(N_wkrs):i_end_parallel]
+	mypart = machine.one_turn_map[N_elements_per_worker*(N_wkrs):i_end_parallel]
 	non_parallel_part = machine.one_turn_map[i_end_parallel:]
-	print 'I am id=%d (master) and my part is %d long'%(myid, len(part_for_master))
+	print 'I am id=%d (master) and my part is %d long'%(myid, len(mypart))
 	
 # initialization bunch
 if I_am_the_master:
@@ -73,13 +98,32 @@ if I_am_the_master:
 	slicer = UniformBinSlicer(n_slices = n_slices, z_cuts=(-z_cut, z_cut))
 	slice_obj_list = bunch.extract_slices(slicer)
 	print 'Bunch sliced.'
+	
 
+# install e-cloud(s)
+if I_am_a_worker or I_am_the_master:
+	import PyECLOUD.PyEC4PyHT as PyEC4PyHT
+	my_new_part = []
+	my_list_eclouds = []
+	for ele in mypart:
+		my_new_part.append(ele)
+		if ele in machine.transverse_map:
+			ecloud_new = PyEC4PyHT.Ecloud(L_ecloud=machine.circumference/n_segments, slicer=None , 
+				Dt_ref=10e-12, pyecl_input_folder='../pyecloud_config',
+				chamb_type = chamb_type,
+				x_aper=x_aper, y_aper=y_aper,
+				filename_chm=filename_chm, Dh_sc=Dh_sc,
+				init_unif_edens_flag=init_unif_edens_flag,
+				init_unif_edens=init_unif_edens, 
+				N_mp_max=N_mp_max,
+				nel_mp_ref_0=nel_mp_ref_0,
+				B_multip=B_multip_per_eV*machine.p0/e*c,
+				slice_by_slice_mode=True)
+			my_new_part.append(ecloud_new)
+			my_list_eclouds.append(ecloud_new)
+	mypart = my_new_part
 
-# initialize savings
-if I_am_the_master:
-	beam_x, beam_y, beam_z = [], [], []
-	sx, sy, sz = [], [], []
-	epsx, epsy, epsz = [], [], []
+comm.Barrier() # only for stdoutp
 
 # simulation
 if I_am_the_master:
@@ -89,7 +133,11 @@ if I_am_the_master:
 	N_pieces = len(pieces_to_be_treated)
 	pieces_treated = []
 	i_turn = 0
-	piece_to_send = None	
+	piece_to_send = None
+	
+	print 'Sim. start'
+	
+	t_last_turn = time.mktime(time.localtime()) 	
 	
 	while True:	
 		orders_from_master = []
@@ -108,7 +156,7 @@ if I_am_the_master:
 
 		
 		if piece_received is not None:
-			for ele in part_for_master: 
+			for ele in mypart: 
 				ele.track(piece_received)
 			pieces_treated.append(piece_received)
 		
@@ -118,34 +166,30 @@ if I_am_the_master:
 			
 			# re-merge bunch
 			bunch = sum(pieces_treated)
-
+			
 			# finalize present turn (with non parallel part)
 			for ele in non_parallel_part:
 				ele.track(bunch)
-				
-			# save results
-			beam_x.append(bunch.mean_x())
-			beam_y.append(bunch.mean_y())
-			beam_z.append(bunch.mean_z())
-			sx.append(bunch.sigma_x())
-			sy.append(bunch.sigma_y())
-			sz.append(bunch.sigma_z())
-			epsx.append(bunch.epsn_x()*1e6)
-			epsy.append(bunch.epsn_y()*1e6)
-			epsz.append(bunch.epsn_z())
 			
 			# prepare next turn
 			pieces_to_be_treated = bunch.extract_slices(slicer)
-			pieces_treated = []			
-								
-			print i_turn
-								
+			pieces_treated = []		
+			
+			t_now = time.mktime(time.localtime())
+			print 'Turn %d, %d s'%(i_turn,t_now-t_last_turn) 
+			t_last_turn = t_now
+												
 			i_turn+=1
 			# check if stop is needed
+			orders_from_master.append('reset_clouds')
 			if i_turn == N_turns: orders_from_master.append('stop')
 			
+		
 		buforders = ch.list_of_strings_2_buffer(orders_from_master)
 		comm.Bcast(buforders, master_id)
+		
+		if 'reset_clouds' in orders_from_master:
+			for ec in my_list_eclouds: ec.finalize_and_reinitialize()
 		
 		if 'stop' in orders_from_master:
 			break
@@ -163,13 +207,13 @@ else: # workers
 			left = myid-1
 		
 		right = myid+1
-
+		
 		sendbuf = ch.beam_2_buffer(piece_to_send)	
 		comm.Sendrecv(sendbuf, dest=right, sendtag=right, 
 		 recvbuf=buf_float, source=left, recvtag=myid)
 		piece_received = ch.buffer_2_beam(buf_float)
-
 		
+				
 		# if you get something do your job
 		if piece_received is not None:
 			for ele in mypart: 
@@ -178,62 +222,15 @@ else: # workers
 		# prepare for next iteration
 		piece_to_send = piece_received
 		
-
 		comm.Bcast(buf_int, master_id)
 		orders_from_master = ch.buffer_2_list_of_strings(buf_int)
 		
+		orders_from_master = comm.bcast(None, root=master_id)
+		
+		if 'reset_clouds' in orders_from_master:
+			for ec in my_list_eclouds: ec.finalize_and_reinitialize()
+		
 		if 'stop' in orders_from_master:
-			break
+			break	
 	
-	
-# output plots
-if False and I_am_the_master:
-	import pylab as plt
-	
-	plt.figure(2, figsize=(16, 8), tight_layout=True)
-	plt.subplot(2,3,1)
-	plt.plot(beam_x)
-	plt.ylabel('x [m]');plt.xlabel('Turn')
-	plt.gca().ticklabel_format(style='sci', scilimits=(0,0),axis='y')
-	plt.subplot(2,3,2)
-	plt.plot(beam_y)
-	plt.ylabel('y [m]');plt.xlabel('Turn')
-	plt.gca().ticklabel_format(style='sci', scilimits=(0,0),axis='y')
-	plt.subplot(2,3,3)
-	plt.plot(beam_z)
-	plt.ylabel('z [m]');plt.xlabel('Turn')
-	plt.gca().ticklabel_format(style='sci', scilimits=(0,0),axis='y')
-	plt.subplot(2,3,4)
-	plt.plot(np.fft.rfftfreq(len(beam_x), d=1.), np.abs(np.fft.rfft(beam_x)))
-	plt.ylabel('Amplitude');plt.xlabel('Qx')
-	plt.subplot(2,3,5)
-	plt.plot(np.fft.rfftfreq(len(beam_y), d=1.), np.abs(np.fft.rfft(beam_y)))
-	plt.ylabel('Amplitude');plt.xlabel('Qy')
-	plt.subplot(2,3,6)
-	plt.plot(np.fft.rfftfreq(len(beam_z), d=1.), np.abs(np.fft.rfft(beam_z)))
-	plt.xlim(0, 0.1)
-	plt.ylabel('Amplitude');plt.xlabel('Qz')
-	
-	fig, axes = plt.subplots(3, figsize=(16, 8), tight_layout=True)
-	twax = [plt.twinx(ax) for ax in axes]
-	axes[0].plot(sx)
-	twax[0].plot(epsx, '-g')
-	axes[0].set_xlabel('Turns')
-	axes[0].set_ylabel(r'$\sigma_x$')
-	twax[0].set_ylabel(r'$\varepsilon_y$')
-	axes[1].plot(sy)
-	twax[1].plot(epsy, '-g')
-	axes[1].set_xlabel('Turns')
-	axes[1].set_ylabel(r'$\sigma_x$')
-	twax[1].set_ylabel(r'$\varepsilon_y$')
-	axes[2].plot(sz)
-	twax[2].plot(epsz, '-g')
-	axes[2].set_xlabel('Turns')
-	axes[2].set_ylabel(r'$\sigma_x$')
-	twax[2].set_ylabel(r'$\varepsilon_y$')
-	axes[0].grid()
-	axes[1].grid()
-	axes[2].grid()
-	for ax in list(axes)+list(twax): 
-		ax.ticklabel_format(useOffset=False, style='sci', scilimits=(0,0),axis='y')
-	plt.show()	
+
