a16c68a260d9d2b6cdc7d0546f420a00e1d0081e
giadarol <giovanni.iadarola@cern.ch>
2016-04-18 20:24:36 +0200

    Change configuration

--- test_kicks/000_run_sim_with_eclouds.sh
+++ test_kicks/000_run_sim_with_eclouds.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 # Run Parallel without MPI
-# ../multiprocexec.py -n 3 sim_class=Simulation_with_eclouds.Simulation
+../multiprocexec.py -n 3 sim_class=Simulation_with_eclouds.Simulation
 
 # Run Serial
 #../serialexec.py sim_class=Simulation_with_eclouds.Simulation
 
 # Run MPI
-mpiexec -n 4 ../withmpi.py sim_class=Simulation_with_eclouds.Simulation
+#mpiexec -n 4 ../withmpi.py sim_class=Simulation_with_eclouds.Simulation
