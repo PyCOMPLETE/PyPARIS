7c354068b9636358219b3d815f05a4bc56658a39
giadarol <giovanni.iadarola@cern.ch>
2018-03-20 15:30:21 +0100

    We build a beam... not tested yet

--- 001_try_multiple_rings/000_run_sim_without_ecloud.sh
+++ 001_try_multiple_rings/000_run_sim_without_ecloud.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Run MPI
-mpiexec -n 6 python for000_run_sim.py
+mpiexec -n 9 python for000_run_sim.py
--- 001_try_multiple_rings/Simulation.py
+++ 001_try_multiple_rings/Simulation.py
@@ -19,10 +19,66 @@ class Simulation(object):
         self.N_parellel_rings = 3
 
     def init_all(self):
-        pass
+        from LHC import LHC
+        machine = LHC(machine_configuration='Injection', n_segments=43, D_x=0., 
+                        RF_at='end_of_transverse')
+        self.machine = machine
+        
+       
+        
         
     def init_master(self):
-        return []
+        
+        print('Building the beam!')
+        
+        from scipy.constants import c as clight, e as qe
+        from PyHEADTAIL.particles.slicing import UniformBinSlicer
+        
+        b_spac_s = 25e-9
+        non_linear_long_matching = False
+        
+        intensity = 1e11
+        epsn_x = 2.5e-6
+        epsn_y = 3.5e-6
+        sigma_z = 10e-2
+
+        #Filling pattern: here head is left and tail is right
+        filling_pattern = [1., 0., 0., 1., 1., 1., 0.]
+        macroparticlenumber_track = 100000
+        
+        machine = self.machine
+        
+        bucket_length_m = machine.circumference/(machine.longitudinal_map.harmonics[0])
+        b_spac_m =  b_spac_s*machine.beta*clight
+        b_spac_buckets = np.round(b_spac_m/bucket_length_m)
+
+        if non_linear_long_matching:
+            generate_bunch = machine.generate_6D_Gaussian_bunch_matched
+        else:
+            generate_bunch = machine.generate_6D_Gaussian_bunch
+
+        list_genbunches = []
+        for i_slot, inten_slot in enumerate(filling_pattern):
+            if inten_slot>0:
+                bunch = generate_bunch(macroparticlenumber_track, inten_slot*intensity, epsn_x, epsn_y, sigma_z=sigma_z)
+                bunch.z -= b_spac_buckets*bucket_length_m*i_slot
+                list_genbunches.append(bunch)
+
+        beam = sum(list_genbunches)
+
+        bucket = machine.longitudinal_map.get_bucket(gamma=machine.gamma, mass=machine.mass, charge=machine.charge)
+        # z_beam_center = bucket.z_ufp_separatrix + bucket_length - self.circumference/2.
+
+        # Here the center of the bucket
+        bucket.z_sfp
+
+        # I want to re-separate the bunches
+        buncher = UniformBinSlicer(n_slices = 0, z_sample_points = np.arange(bucket.z_sfp-len(filling_pattern)*bucket_length_m*b_spac_buckets, 
+                                                bucket.z_sfp+bucket_length_m, bucket_length_m*b_spac_buckets))
+        buncher_slice_set = beam.get_slices(buncher, statistics=True)
+        list_bunches = beam.extract_slices(buncher, include_non_sliced='never')
+        
+        return list_bunches
 
     def init_worker(self):
         pass
