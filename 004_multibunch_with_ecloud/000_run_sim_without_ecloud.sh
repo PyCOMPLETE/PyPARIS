817d87b52d9e8bc0d5512c8b14fb084dcfd10055
giadarol <giovanni.iadarola@cern.ch>
2018-07-28 23:09:31 +0200

    multiprocexec now works in multiturn mode

--- 004_multibunch_with_ecloud/000_run_sim_without_ecloud.sh
+++ 004_multibunch_with_ecloud/000_run_sim_without_ecloud.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
 # Run MPI
-mpiexec -n 8 python for000_run_sim.py
+# mpiexec -n 8 python for000_run_sim.py
+
+# Run multiproc
+../multiprocexec.py -n 4 sim_class=Simulation.Simulation --multiturn
 
 # Run single core
 # python for000_run_sim.py
