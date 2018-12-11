9ca56be7f6cc28d76d64092a5e0ad1f7aee900da
giadarol <giovanni.iadarola@cern.ch>
2018-12-11 16:42:44 +0100

    Added example without ecloud and moved things around

---
+++ test_multibunch/001_multibunch_without_ecloud/000_run_sim_without_ecloud.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Run MPI
+mpiexec -n 4 python for000_run_sim.py
+
+# Run multiproc
+# ../../../PyPARIS/multiprocexec.py -n 4 sim_class=Simulation.Simulation --multiturn
+
+# Run single core
+#python for000_run_sim.py
