d8b70fcc34339acab98bb6c62926cb038f39d670
giadarol <giovanni.iadarola@cern.ch>
2018-12-11 18:16:37 +0100

    Working on checks

--- test_multibunch/001_multibunch_without_ecloud/002_some_checks.py
+++ test_multibunch/001_multibunch_without_ecloud/002_some_checks.py
@@ -17,6 +17,8 @@ tau_damp_y = 100.
 ob = mfm.myloadmat_to_obj(tag+'_matrices.mat')
 
 plt.close('all')
+
+# Plot transverse positions
 fig1 = plt.figure(1)
 fig1.set_facecolor('w')
 ms.mystyle_arial(fontsz=16)
@@ -27,18 +29,32 @@ axy = fig1.add_subplot(2,1,2, sharex=axx)
 axx.plot(ob.mean_x[:, i_bunch])
 axy.plot(ob.mean_y[:, i_bunch])
 
+# Plot transverse spectra
+fig2 = plt.figure(2)
+fig2.set_facecolor('w')
+
+axfx = fig2.add_subplot(2,1,1)
+axfy = fig2.add_subplot(2,1,2, sharex=axfx)
+
+spectx = np.abs(np.fft.rfft(ob.mean_x[:, i_bunch]))
+specty = np.abs(np.fft.rfft(ob.mean_y[:, i_bunch]))
+freq = np.fft.rfftfreq(len(ob.mean_x[:, i_bunch]))
+
+axfx.plot(freq, spectx)
+axfy.plot(freq, specty)
+
 if flag_check_damp_time:
     turn_num = np.arange(0, len(ob.mean_x[:, i_bunch]), dtype=np.float)
     axx.plot(ob.mean_x[0, i_bunch]*np.exp(-turn_num/tau_damp_x))
     axy.plot(ob.mean_y[0, i_bunch]*np.exp(-turn_num/tau_damp_y))
 
-for ax in [axx, axy]:
+for ax in [axx, axy, axfx, axfy]:
     ax.ticklabel_format(style='sci', scilimits=(0,0),axis='y')
 
-for ax in [axx, axy]:
+for ax in [axx, axy, axfx, axfy]:
     ax.yaxis.set_major_locator(MaxNLocator(5))
 
-for ax in [axx, axy]:
+for ax in [axx, axy, axfx, axfy]:
     ax.grid(True)
 
 plt.show()
