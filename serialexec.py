cda965807b878a5fa0e22e40347e5e7513bb4c06
giadarol <giovanni.iadarola@cern.ch>
2019-03-13 16:49:54 +0100

    General import in MPI and serial

--- serialexec.py
+++ serialexec.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import os, sys
+import importlib
 
 BIN = os.path.expanduser("./")
 sys.path.append(BIN)
@@ -14,14 +15,14 @@ if 'sim_class' not in sys.argv[1]:
 sim_module_string = sys.argv[1].split('=')[-1]
 
 sim_module_strings = sim_module_string.split('.')
-if len(sim_module_strings)!=2:
-    raise(ValueError('\n\nsim_class must be given in the form: module.class.\nNested referencing not implemented.\n\n'))
-module_name = sim_module_strings[0]
-class_name = sim_module_strings[1]
-    
 
-SimModule = __import__(module_name)
-SimClass = getattr(SimModule, class_name)
+# if len(sim_module_strings)!=2:
+#     raise(ValueError('\n\nsim_class must be given in the form: module.class.\nNested referencing not implemented.\n\n'))
+module_name = '.'.join(sim_module_strings[:-1])
+class_name = sim_module_strings[-1]
+    
+SimModule = importlib.import_module(module_name)
+SimClass = getattr(SimModule, class_name)    
 
 from PyPARIS.ring_of_CPUs import RingOfCPUs
 simulation_content = SimClass()
--- withmpi.py
+++ withmpi.py
@@ -2,6 +2,7 @@
 import multiprocessing as mp
 import numpy as np
 import os, sys
+import importlib
 
 sys.path.append('./')
 	
@@ -12,15 +13,15 @@ if 'sim_class' not in sys.argv[1]:
 
 sim_module_string = sys.argv[1].split('=')[-1]
 
-sim_module_strings = sim_module_string.split('.')
-if len(sim_module_strings)!=2:
-    raise(ValueError('\n\nsim_class must be given in the form: module.class.\nNested referencing not implemented.\n\n'))
-module_name = sim_module_strings[0]
-class_name = sim_module_strings[1]
-    
 
-SimModule = __import__(module_name)
-SimClass = getattr(SimModule, class_name)
+sim_module_strings = sim_module_string.split('.')
+# if len(sim_module_strings)!=2:
+#     raise(ValueError('\n\nsim_class must be given in the form: module.class.\nNested referencing not implemented.\n\n'))
+module_name = '.'.join(sim_module_strings[:-1])
+class_name = sim_module_strings[-1]
+    
+SimModule = importlib.import_module(module_name)
+SimClass = getattr(SimModule, class_name)    
 
 from PyPARIS.ring_of_CPUs import RingOfCPUs
 simulation_content = SimClass()
